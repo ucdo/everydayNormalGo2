4d67cc666cc957bf5fa4ec65842ff0d3ae53414e
ucdo <phpsarc@gmail.com>
2024-09-02 18:02:22 +0800

    learn fyne
    learn input, canvas

--- Desktop/README.md
+++ Desktop/README.md
@@ -4,9 +4,10 @@
 
 ## 运行
 1. 定义 一个app  appx:= app.New() . 额不能叫app因为和导入的包同名了
-2. 执行时 appx.Run()
+2. 执行时 window.ShowAndRun()  appx.Run()
 3. 定义窗口 window := appx.NewWindow("window name")
 4. 往窗口里写内容 window.SetContent(widget.NewLabel("hello"))
 5. window.Show()展示窗口，放在appx.Run() 的前面
 6. 设置PC端窗口的大小 window.Resize(fyne.NewSize(100,100)) // 移动端不搞这个，因为仅仅以全屏展示
 7. 经过测试发现 widget.NewButton().Resize() 不起作用
+8. 
--- Desktop/main.go
+++ Desktop/main.go
@@ -4,11 +4,25 @@ import (
 	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"image/color"
 	"time"
 )
 
+var ticker int
+
 func main() {
+	ticker = 0
+	drawRectangle()
+}
+
+func tidyUp() {
+	fmt.Println("Exit!")
+}
+
+func window2() {
 	appx := app.New()
 	window := appx.NewWindow("Hello") // 这个是窗口的title
 	window.Resize(fyne.NewSize(300, 300))
@@ -49,11 +63,71 @@ func main() {
 	tidyUp()
 }
 
-func tidyUp() {
-	fmt.Println("Exit!")
-}
-
 func updateTime(clock *widget.Label) {
 	format := time.Now().Format("2006-01-02 15:04:05")
 	clock.SetText(format)
+}
+
+func addInput() {
+	a := app.New()
+	w := a.NewWindow("Hello Person")
+
+	w.SetContent(container.NewVBox(makeUI()))
+	w.ShowAndRun()
+}
+
+func makeUI() (*widget.Label, *widget.Entry) {
+	out := widget.NewLabel("Hello motherfucker")
+	in := widget.NewEntry()
+
+	in.OnChanged = func(s string) {
+		out.SetText("Hello " + s + "!")
+	}
+
+	return out, in
+}
+
+func drawRectangle() {
+	appx := app.New()
+	myWindow := appx.NewWindow("draw")
+	myCanvas := myWindow.Canvas()
+
+	blue := color.NRGBA{R: 0, G: 0, B: 180, A: 255}
+
+	rect := canvas.NewRectangle(blue)
+	myCanvas.SetContent(rect)
+
+	for i := 0; i < 10; i++ {
+		go func(myCanvas fyne.Canvas) {
+			time.Sleep(1 * time.Second)
+			//green := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
+			//rect.FillColor = green
+			if ticker%2 == 0 {
+				setContext(myCanvas)
+			} else {
+				setContentToCircle(myCanvas)
+			}
+			ticker++
+			rect.Refresh()
+		}(myCanvas)
+		time.Sleep(1 * time.Second)
+	}
+
+	myWindow.Resize(fyne.NewSize(100, 100))
+	myWindow.ShowAndRun()
+}
+
+func setContext(c fyne.Canvas) {
+	green := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
+	text := canvas.NewText("text", green)
+	text.TextStyle.Bold = true
+	c.SetContent(text)
+}
+
+func setContentToCircle(c fyne.Canvas) {
+	red := color.NRGBA{R: 0xff, G: 0x33, B: 0x33, A: 0xff}
+	circle := canvas.NewCircle(red)
+	circle.StrokeWidth = 4
+	circle.StrokeColor = red
+	c.SetContent(circle)
 }
